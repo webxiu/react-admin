f0778c38c010947e3060978678b6b9fef711985d
XiuHai.Li <759430324@qq.com>
2019-11-20 13:46:15 +0800

    封装表单

--- src/components/common/BaseForm/index.js
+++ src/components/common/BaseForm/index.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
-import { Input, Select, Form, Button, Checkbox, Radio } from "antd";
+import { Input, Select, Form, Button, Checkbox, DatePicker, Radio, Upload, Icon } from "antd";
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+moment.locale('zh-cn');
 const FormItem = Form.Item
 const Option = Select.Option
+const CheckboxGroup = Checkbox.Group
+const RadioGroup = Radio.Group
+const { TextArea } = Input;
+const { MonthPicker, RangePicker } = DatePicker;
+const UploadDragger = Upload.Dragger;
 
 class BaseForm extends Component {
     state = {
-        layout: this.props.formData.layout
+        formConfig: this.props.formData.formConfig
     }
 
     // 表表单项渲染
@@ -19,11 +27,19 @@ class BaseForm extends Component {
                 let label = item.label
                 let field = item.field
                 let initialValue = item.initialValue
-                let defaultValue = item.defaultValue
                 let placeholder = item.placeholder
                 let width = item.width
                 let rules = item.rules
                 let allowClear = item.allowClear
+                let dateFormat = item.dateFormat
+                let autoSize = item.autoSize
+                // 上传
+                let extra = item.extra
+                let action = item.action
+                let accept = item.accept
+                let listType = item.listType
+                let uploadTitle = item.uploadTitle
+                let multiple = item.multiple
 
 
                 if (item.type === 'INPUT') {
@@ -47,14 +63,14 @@ class BaseForm extends Component {
                     const SELECT = <FormItem label={label} key={field}>
                         {
                             getFieldDecorator([field], {
-                                initialValue: initialValue,
+                                // initialValue: initialValue,
                                 rules: rules
                             })(
                                 <Select
                                     style={{ width: width }}
                                     placeholder={placeholder}
                                 >
-                                    {this.getOptionList(item.list)}
+                                    {this.getSelectList(item.list)}
                                 </Select>
                             )
                         }
@@ -64,29 +80,141 @@ class BaseForm extends Component {
                     const CHECKBOX = <FormItem label={label} key={field}>
                         {
                             getFieldDecorator([field], {
-                                valuePropName: 'checked',
+                                // valuePropName: 'checked',
                                 initialValue: initialValue,// 必须是true/false
                                 rules: rules
                             })(
-                                <Checkbox
+                                <CheckboxGroup
                                     style={{ width: width }}
-                                >{label}</Checkbox>
+                                >
+                                    {this.getCheckboxGroup(item.list)}
+                                </CheckboxGroup>
                             )
                         }
                     </FormItem>
                     formItemList.push(CHECKBOX)
+
+                } else if (item.type === 'RADIO') {
+                    const RADIO = <Form.Item label={label} key={field}>
+                        {getFieldDecorator([field], {
+                            initialValue: initialValue,
+                            rules: rules
+                        })(
+                            <RadioGroup
+                                style={{ width: width }}
+                            >
+                                {this.getRadioGroup(item.list)}
+                            </RadioGroup>
+                        )}
+                    </Form.Item>
+                    formItemList.push(RADIO)
+
+                } else if (item.type === 'RANGEPICKER') {
+                    const RANGEPICKER = <FormItem label={label} key={field}>
+                        {getFieldDecorator([field], {
+                            rules: rules,
+                            initialValue: [moment(initialValue[0], dateFormat), moment(initialValue[1], dateFormat)]
+                        })(<RangePicker
+                            placeholder={placeholder}
+                            style={{ width: width }}
+                            format={dateFormat}
+                        />)}
+                    </FormItem>
+                    formItemList.push(RANGEPICKER)
+                } else if (item.type === 'TEXTAREA') {
+                    const TEXTAREA = <FormItem label={label} key={field}>
+                        {
+                            getFieldDecorator([field], {
+                                initialValue: initialValue,
+                                rules: rules,
+                            })(
+                                <TextArea
+                                    style={{ width: width }}
+                                    placeholder={placeholder}
+                                    allowClear={allowClear}
+                                    autoSize={autoSize}
+                                />
+                            )
+                        }
+                    </FormItem>
+                    formItemList.push(TEXTAREA)
+                } else if (item.type === 'UPLOAD') {
+                    const UPLOAD = <FormItem label={label} extra={extra}>
+                        {getFieldDecorator([field], {
+                            valuePropName: 'fileList',
+                            getValueFromEvent: this.normFile,
+                            rules: rules
+                        })(
+                            <Upload
+                                width={width}
+                                name={field}
+                                action={action}
+                                accept={accept}
+                                multiple={multiple}
+                                listType={listType}>
+                                <Button>
+                                    <Icon type="upload" />{uploadTitle}
+                                </Button>
+                            </Upload>,
+                        )}
+                    </FormItem>
+                    formItemList.push(UPLOAD)
+                } else if (item.type === 'DRAGGER') {
+                    const DRAGGER = <FormItem label={label} extra={extra}>
+                        {getFieldDecorator([field], {
+                            valuePropName: 'fileList',
+                            getValueFromEvent: this.normFile,
+                        })(
+                            <UploadDragger
+                                width={width}
+                                name={field}
+                                action={action}
+                                accept={accept}
+                                multiple={multiple}
+                                listType={listType}
+                            >
+                                <p className="ant-upload-drag-icon">
+                                    <Icon type="inbox" />
+                                </p>
+                                <p className="ant-upload-text">{uploadTitle}</p>
+                                <p className="ant-upload-hint">{extra}</p>
+                            </UploadDragger>,
+                        )}
+                    </FormItem>
+                    formItemList.push(DRAGGER)
                 }
             })
         }
         return formItemList
     }
-    // 处理option
-    getOptionList(list) {
+    // 处理select option
+    getSelectList(list) {
         if (!list) {
             return []
         }
         return list.map(item => <Option value={item.value} key={item.value}>{item.name}</Option>)
     }
+    // 处理checkbox group
+    getCheckboxGroup(group) {
+        if (!group) {
+            return []
+        }
+        return group.map(item => <Checkbox value={item.value} key={item.value}>{item.name}</Checkbox>)
+    }
+    // 处理radio group
+    getRadioGroup(group) {
+        if (!group) {
+            return []
+        }
+        return group.map(item => <Radio value={item.value} key={item.value}>{item.name}</Radio>)
+    }
+    normFile = e => {
+        console.log('上传事件:', e);
+        if (Array.isArray(e)) {
+            return e;
+        }
+        return e && e.fileList;
+    };
     // 提交
     submitHandle = (e) => {
         e.preventDefault();
@@ -106,7 +234,7 @@ class BaseForm extends Component {
     }
     render() {
         return (
-            <Form layout={this.state.layout}>
+            <Form {...this.state.formConfig}>
                 {/* 渲染表单 */}
                 {this.initFormList()}
                 <FormItem>
--- src/pages/userForm/index.js
+++ src/pages/userForm/index.js
@@ -10,7 +10,12 @@ export default class Form extends Component {
         page: 1
     }
     formData = {
-        layout: 'inline',//'horizontal'|'vertical'|'inline'
+        formConfig:{
+            layout: 'horizontal',//'horizontal'|'vertical'|'inline', inline会默认添加冒号
+            hideRequiredMark: true,//是否隐藏*号
+            labelAlign: 'left',//'left' | 'right'
+            colon:false, // 添加标题的冒号,只有在horizontal下生效
+        },
         formList: [
             {
                 type: 'INPUT',
@@ -30,9 +35,9 @@ export default class Form extends Component {
                 placeholder: '请选择类型3333',
                 width: 200,
                 list: [
-                    { value: '0', name: '全部' },
-                    { value: '1', name: '未处理' },
-                    { value: '2', name: '已处理' }
+                    { value: 0, name: '全部' },
+                    { value: 1, name: '未处理' },
+                    { value: 2, name: '已处理' }
                 ],
                 rules: [{ required: true, message: '请输入用户名!' }],
             },
@@ -40,8 +45,77 @@ export default class Form extends Component {
                 type: 'CHECKBOX',
                 label: '操作类型',
                 field: 'instrest',
-                initialValue: true,
+                initialValue: ['0','2'],
                 width: 200,
+                list: [
+                    { value: '0', name: '唱歌' },
+                    { value: '1', name: '跳舞' },
+                    { value: '2', name: '爬山' }
+                ],
+                rules: [{ required: true, message: '请选择兴趣' }]
+            },
+            {
+                type: 'RADIO',
+                label: '性别',
+                field: 'sex',
+                initialValue: 1,
+                // initialValue: '',
+                width: 200,
+                list: [
+                    { value: 0, name: '男' },
+                    { value: 1, name: '女' },
+                    { value: 2, name: '保密' }
+                ],
+                rules: [{ required: true, message: '请选择性别!' }]
+            },
+            {
+                type: 'RANGEPICKER',
+                label: '查询日期',
+                field: 'date',
+                width: 260,
+                placeholder: ['开始时间', '时间结束'],
+                dateFormat: 'YYYY/MM/DD',
+                initialValue: ['2019-11-19', '2019-11-20'],
+                rules: [{ type: 'array', required: true, message: '请选择时间' }],
+            },
+            {
+                type: 'TEXTAREA',
+                label: '发布文章',
+                field: 'content',
+                allowClear: true,
+                initialValue: '我是默认值...',
+                width: 280,
+                placeholder: '请输入内容96',
+                autoSize:{ minRows: 2, maxRows: 26 },
+                rules: [{ required: true, message: '请输入文章' }],
+            },
+            {
+                // 点击上传
+                type: 'UPLOAD',
+                label: '点击上传文件',
+                multiple: true,
+                field: 'upload',// 发到后台的参数名
+                extra: '详情说明信息...',
+                uploadTitle: '点击上传',
+                action: '/upload/img',
+                accept: '.jpg,.png,.gif.doc.exe',//.doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document
+                listType: 'picture',//列表的内建样式 text, picture picture-card
+                width: 280,
+                rules: [{ required: true, message: '请选择文件...' }],
+            },
+            {
+                // 拖动 + 点击上传
+                type: 'DRAGGER',
+                label: '点击拖拽上传文件',
+                multiple: true,
+                field: 'dragger',// 发到后台的参数名
+                extra: '支持单个或批量上传。',
+                uploadTitle: '点击 或 拖拽文件到此处上传',
+                action: '/upload/img',
+                accept: '.jpg,.png,.gif.doc.exe',//.doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document
+                listType: 'picture',//列表的内建样式 text, picture picture-card
+                width: 280,
+                rules: [{ required: true, message: '请选择文件...' }],
             }
         ]
     }
